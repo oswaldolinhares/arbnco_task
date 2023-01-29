affb209ceb2495be2acc96f7f7e6dcc4fd244cec
Oswaldo Linhares <oswaldolinhares@gmail.com>
2023-01-29 09:59:04 -0300

    feat(Hstore): enable hstore

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_28_130658) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_28_172217) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "hstore"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
