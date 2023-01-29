50e3c8504e326c89706243ed1136106c37a4471a
Oswaldo Linhares <oswaldolinhares@gmail.com>
2023-01-29 16:09:06 -0300

    feat(InpToHash): converter of inp files to array of hashes

---
+++ app/services/convert_file/inp_to_hash.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+module ConvertFile
+  class InpToHash < ApplicationService
+    attr_reader :inp_file, :objects, :user_submission
+
+    FILE_SPLIT_REGEX = /\s*\.{2}\n\n*/
+    EXTRACT_GROUPS_REGEX = /"(.+)"\s+=\s+([^\n]*)\n*\s*(.+)?/m
+
+    def initialize(inp_file, user_submission)
+      super()
+      @inp_file = inp_file
+      @user_submission = user_submission
+      @objects = []
+    end
+
+    def call
+      process_inp_file
+
+      @objects
+    end
+
+    private
+
+    def generate_json
+      objects.to_json
+    end
+
+    def process_inp_file
+      dowloaded_file = inp_file.download
+
+      dowloaded_file.split(FILE_SPLIT_REGEX).each do |file_section|
+        name, object, properties = file_section.match(EXTRACT_GROUPS_REGEX).captures
+        properties = format_properties(properties) unless properties.nil?
+
+        objects << { name: name,
+                     object_type: get_object_type(object),
+                     user_submission: user_submission,
+                     properties: { name.to_s => object.to_s }.merge(properties) }
+      end
+
+      # dowloaded_file.split(FILE_SPLIT_REGEX).each do
+      #   name, object, properties = _1.match(EXTRACT_GROUPS_REGEX).captures
+      #   properties = format_properties(properties) unless properties.nil?
+
+      #   objects << { name: name,
+      #                object_type: get_object_type(object),
+      #                user_submission: user_submission,
+      #                properties: { name.to_s => object.to_s }.merge(properties) }
+      # end
+    end
+
+    def get_object_type(object)
+      ObjectType.find_by(name: object) || ObjectType.last
+    end
+
+    def format_properties(properties)
+      to_hash(format_elements(split_properties(properties)))
+    end
+
+    def split_properties(properties)
+      properties.split("\n")
+    end
+
+    def format_elements(elements)
+      elements.map do |e|
+        e.gsub(/\s+/, "").delete('"').split("=").map(&:strip)
+      end
+    end
+
+    def to_hash(formatted_elements)
+      formatted_elements.to_h { |e| e.size == 1 ? [e[0], ""] : e }
+    end
+  end
+end
--- spec/factories/object_types.rb
+++ spec/factories/object_types.rb
@@ -5,5 +5,14 @@ FactoryBot.define do
     name do
       %w[WALL WINDOW ZONE HVAC-SYSTEM DHW-GENERATOR GLASS CONSTRUCTION COMPLIANCE GENERAL REC-USER UNIDENTIFIED].sample
     end
+    trait :objects_type_list do
+      objects_list = %w[WALL WINDOW ZONE HVAC-SYSTEM DHW-GENERATOR GLASS CONSTRUCTION COMPLIANCE GENERAL REC-USER
+                        UNIDENTIFIED]
+      objects_list.each do |object|
+        after(:create) do |_instance|
+          create_list(:object_type, 11, name: object)
+        end
+      end
+    end
   end
 end
